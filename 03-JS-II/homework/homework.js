4ceba481b29d2e0da755b1d0d411d1810783c817
xx5SOSxx <ocarlosraul074@gmail.com>
2022-06-15 18:23:38 -0500

    Primer guardado. 23 completas

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,24 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x === y) {
+    return x;
+  } else if (x > y) {
+    return x;
+  }
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if(edad >= 18) {
+    return "Allowed";
+  }
+  else {
+    return "Not allowed";
+  }
 }
   
 function conection(status) {
@@ -19,6 +31,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return 'Online';
+  } else if (status === 2) {
+    return 'Away';
+  }
+  return 'Offline';
 }
 
 function saludo(idioma) {
@@ -28,6 +46,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === 'aleman') {
+    return 'Guten Tag!';
+  } else if (idioma === 'mandarin') {
+    return 'Ni Hao!';
+  } else if (idioma === 'ingles') {
+    return 'Hello!';
+  } else {
+    return 'Hola!';
+  }
 }
 
 function colors(color) {
@@ -38,18 +65,40 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case 'blue':
+      return 'This is blue';
+    case 'red':
+      return 'This is red';
+    case 'green':
+      return 'This is green';
+    
+    case 'orange':
+      return 'This is orange';
+      
+    default:
+      return 'Color not found';
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  }
+  return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) {
+    return true;
+  }
+  return false;
 }
 
 function esEntero(numero) {
@@ -60,6 +109,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  
 }
 
 function fizzBuzz(numero) {
@@ -67,6 +117,7 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -75,7 +126,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return 'Error';
+  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return 'Hay negativos';
+  } else if (num1 > 0 && num1 > num2 && num1 > num3) {
+    return 'Número 1 es mayor y positivo'
+  } else if (num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  } else 
+  return false;
 }
 
 function esPrimo(numero) {
@@ -90,6 +151,12 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if(valor === true){
+    return 'Soy verdadero';
+  } else {
+    return 'Soy falso';
+  }
+
 
 }
 
